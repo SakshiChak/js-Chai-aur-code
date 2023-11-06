800a13dd5c26c728cc739860a8cd916db664da5a
SakshiChak <129160461+SakshiChak@users.noreply.github.com>
2023-11-06 10:11:27 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -43,7 +43,7 @@ let isLoggedIn = true
 let isEven = 1;
 
 let booleanIsEven = Boolean(isEven);
-console.log(booleanIsEven);//true
+// console.log(booleanIsEven);//true
 
 /* Convert into boolean */
 // 1 => true; 0 => false
@@ -53,5 +53,67 @@ console.log(booleanIsEven);//true
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// <--------------------- Operations ------------------------>
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(4/2);
+// console.log(5%2);
+
+let str1 = "hello"
+let str2 = " sakshi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32
+
+// console.log((3 + 4) * 5 % 3);
+
+/* not prefer to use this */
+// console.log(+true);//1
+// console.log(+"");//0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2; //inconsistence code
+
+//<-----------Prefix & Postfix Operation------------>
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);//101
+
+let gameScore = 100
+++gameScore;
+console.log(gameScore);//101
+
+let x = 3;
+const y = x++;//Postfix
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;//Prefix
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
