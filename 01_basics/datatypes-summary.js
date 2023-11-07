97b8011438a7a6bde6cb8f3f7a0ddefd457fe2a4
SakshiChak <129160461+SakshiChak@users.noreply.github.com>
2023-11-07 09:44:24 +0000

    dataTypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -42,6 +42,7 @@ const myFunction = function () {
 // myFunction();//func call
 console.log(typeof myFunction);//function
 
+// link to study
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
 
