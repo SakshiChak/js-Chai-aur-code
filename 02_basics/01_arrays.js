9f6f0c19fe68f038ea857e5825959dbaebae37fa
Sakshi Chak <129160461+SakshiChak@users.noreply.github.com>
2023-11-09 10:08:52 +0000

    add notes of slice and splice

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -10,25 +10,26 @@ const myArr2 = new Array(1, 2, 3, 4);
 // Accessing array elements
 console.log(myArr[1]);
 
-// Array methods
+// <-----------------Array methods--------------->
 
 // Adding and removing elements from the end of an array
 myArr.push(6);
 myArr.push(7);
 myArr.pop();
+console.log(myArr);//[0, 1, 2, 3, 4, 5, 6]
 
 // Adding and removing elements from the beginning of an array
 
 // Add an element to the beginning of the array
 myArr.unshift(9);
-console.log(myArr);//[9, 0, 1, 2,3, 4, 5, 6]
+console.log(myArr);//[9, 0, 1, 2, 3, 4, 5, 6]
 
 // Remove the first element from the array
 myArr.shift();
 console.log(myArr);//[0, 1, 2, 3,4, 5, 6]
 
 // Checking if an array includes a specific element
-console.log(myArr.includes(9));
+console.log(myArr.includes(9));//false
 
 // Finding the index of a specific element in an array
 console.log(myArr.indexOf(3));
@@ -39,7 +40,7 @@ console.log(newArr);//0,1,2,3,4,5
 
 // Slice and Splice
 
-console.log("A ", myArr);//[ 0, 1, 2, 3, 4, 5 ]
+console.log("Original Array:", myArr);//[ 0, 1, 2, 3, 4, 5 ]
 
 // Slice: Creating a new array by extracting a portion of the original array
 const myNew1 = myArr.slice(1, 3);
@@ -50,3 +51,18 @@ console.log("Original Array (Unchanged):", myArr);//[ 0, 1, 2, 3, 4, 5 ]
 const myNew2 = myArr.splice(1, 3);
 console.log("Splice Result (Removed Elements):", myNew2);// [ 1, 2, 3 ]
 console.log("Original Array (Modified):", myArr);//[ 0, 4, 5 ]
+
+// <-------------key points of slice and splice------>
+/*
+slice:
+
+1.Returns a shallow copy of a portion of an array.
+2.Does not modify the original array; creates a new one.
+3.Takes two arguments: start index and end index (end index is exclusive).
+
+splice:
+
+1.Changes the contents of an array by removing or replacing existing elements and/or adding new elements.
+2.Modifies the original array in place.
+3.Takes at least two arguments: start index and number of elements to remove.
+*/
