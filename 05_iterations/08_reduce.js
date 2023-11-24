c43876812243b57d3552df797e55691ecba89278
Sakshi Chak <129160461+SakshiChak@users.noreply.github.com>
2023-11-24 17:59:43 +0000

    reduce

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -1,5 +1,5 @@
 // Example 1: Summing up an array of numbers using reduce
-const myNums = [1, 2, 3]
+const myNums = [1, 2, 3];
 
 // const myTotal = myNums.reduce(function (acc, currval) {
 //     console.log(`acc: ${acc} and currval: ${currval}`);
